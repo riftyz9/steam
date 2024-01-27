fdd1eb0b7c0c995f313318b0974948004e1dd1b7
riftyz9 <96852581+riftyz9@users.noreply.github.com>
2024-01-27 01:17:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 578080, 304930];  // Enter here AppIDs of the needed games
+var games = [578080, 730, 304930];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
